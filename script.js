bb09faf904c2721a7fad40ba83a3e725d97520fc
sharena <144714984+sharenaa@users.noreply.github.com>
2024-03-13 15:13:55 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function myfunction() {
+document.getElementById("greeting").style.color="lavender";
+}
+function myfunction2(){
+  document.body.style.backgroundColor="lightgreen";
+}
+function myfunction3(){
+  document.getElementById("reflect").style.fontFamily="Arial";
+}
